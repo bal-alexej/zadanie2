9cb10af4d99f15432fc6cf4bc85149793d744968
bal-alexej <56967506+bal-alexej@users.noreply.github.com>
2020-09-19 16:17:04 +0300

    Усатновил webpack

---
+++ src/analitics.js
@@ -0,0 +1,13 @@
+function craateAnalytics() {
+  let counter = () => counter++
+
+  document.addEventListener(type: 'click', listener)
+
+
+return{
+  destroy(){
+    document.removaEventListener(type: 'click', listener)
+  }  
+}
+
+}
